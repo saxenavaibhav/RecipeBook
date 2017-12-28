673637a3b9609668d9c62dcccfef38914cf95cc6
saxenavaibhav <reachvaibhavsaxena@gmail.com>
2017-12-28 12:30:55 +0530

    IndexController.java Updated

--- src/main/java/com/saxena/vaibhav/controller/IndexController.java
+++ src/main/java/com/saxena/vaibhav/controller/IndexController.java
@@ -12,7 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 public class IndexController {
 	
-	private RecipeService recipeService1;
+	private RecipeService recipeService;
 	
 	public IndexController(RecipeService recipeService) {
 		this.recipeService = recipeService;
